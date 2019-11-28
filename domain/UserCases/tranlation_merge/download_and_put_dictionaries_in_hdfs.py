5d6ca8ce6bd0ea23c77c935fd8772ba8bbdd2fb2
Andres <Cevallos>
2019-11-28 12:00:33 +0100

    fixed download files script

---
+++ domain/UserCases/tranlation_merge/download_and_put_dictionaries_in_hdfs.py
@@ -0,0 +1,23 @@
+import urllib.request
+import os
+from subprocess import PIPE, Popen
+
+languages = ["German", "French", "Spanish", "Italian"]
+
+dirPath = os.getcwd()
+downloadPath = os.path.join(dirPath, 'data')
+
+for language in languages:
+    print('Downloading dictionary: ' + language)
+    fileName = language + '.txt'
+    url = 'http://www.ilovelanguages.com/IDP/files/' + fileName
+    downloadPath = os.path.join(downloadPath, fileName)
+    urllib.request.urlretrieve(url, downloadPath)
+
+fullDictionary = downloadPath+"dictionary.txt"
+os.system("cat "+downloadPath+"/* >> "+fullDictionary)
+
+print('Moving dictionary to HDFS via PUT command')
+hdfsPath = '/tmp/' + fullDictionary
+put = Popen(["hadoop", "fs", "-put", downloadPath, hdfsPath], stdin=PIPE, bufsize=-1)
+put.communicate()
